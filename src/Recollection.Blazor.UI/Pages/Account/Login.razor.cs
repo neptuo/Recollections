7f1704e919edf5ca5e05d03818bfb0516132fe92
Marek Fišera <mara@neptuo.com>
2019-05-25 19:10:51 +0200

    #2 - Login form.

---
+++ src/Recollection.Blazor.UI/Pages/Account/Login.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Recollection.Pages.Account
+{
+    public class LoginModel : ComponentBase
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        protected async Task LoginAsync()
+        {
+            Console.WriteLine($"Login as '{Username}'.");
+        }
+    }
+}
