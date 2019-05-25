8958acc59a46c6221c482b50c4aff5d93a86b02e
Marek Fišera <mara@neptuo.com>
2019-05-25 18:13:03 +0200

    #1 - Blazor UI project.

---
+++ src/Recollection.Blazor.UI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neptuo.Recollection
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
