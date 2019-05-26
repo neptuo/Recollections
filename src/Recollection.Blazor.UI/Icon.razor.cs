26be4ec670c84cd56aa6c0aad2c6e1df84a231a3
Marek Fišera <mara@neptuo.com>
2019-05-26 11:34:32 +0200

    #5 - Open Iconic.

---
+++ src/Recollection.Blazor.UI/Icon.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Recollection
+{
+    public class IconModel : ComponentBase
+    {
+        [Parameter]
+        protected string Identifier { get; set; }
+    }
+}
