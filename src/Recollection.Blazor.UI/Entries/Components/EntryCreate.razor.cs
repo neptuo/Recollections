89f16e323615a4f66b6a30d7b6f3d36ddf525654
Marek Fišera <mara@neptuo.com>
2019-05-27 18:05:54 +0200

    #3 - UI basics for timeline view.

---
+++ src/Recollection.Blazor.UI/Entries/Components/EntryCreate.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Recollection.Entries.Components
+{
+    public class EntryCreateModel : ComponentBase
+    {
+        public string Title { get; set; }
+        public DateTime When { get; set; }
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+
+            When = DateTime.Today;
+        }
+
+        public async Task CreateAsync()
+        {
+            Title = null;
+            When = DateTime.Today;
+        }
+    }
+}
---
+++ src/Recollection.Blazor.UI/Entries/Pages/Timeline.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Recollection.Entries.Pages
+{
+    public class TimelineModel : ComponentBase
+    {
+        public List<TimelineEntryModel> Entries { get; } = new List<TimelineEntryModel>();
+
+        protected async override Task OnInitAsync()
+        {
+            await base.OnInitAsync();
+
+            Entries.Add(new TimelineEntryModel()
+            {
+                Title = "Tenerife 2018",
+                When = new DateTime(2018, 9, 27),
+                Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec odio dictum lorem aliquet dignissim eu a ex. Integer ac fermentum risus. Maecenas consequat dolor non eleifend interdum. Quisque nec pretium felis. Proin vitae vestibulum purus, sit amet hendrerit nisl. Vivamus at elementum tellus. Duis non urna ut sapien dignissim feugiat. Nullam laoreet libero eget dolor auctor luctus. Mauris elit massa, elementum eget aliquet ac, molestie porta sapien. Suspendisse metus justo, feugiat ut dignissim vel, maximus nec ex. Nulla sagittis urna vitae ornare dictum."
+            });
+        }
+    }
+}
