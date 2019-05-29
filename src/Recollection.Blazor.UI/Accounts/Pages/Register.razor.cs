34478328c564f3b452dac35161bf174e198865c3
Marek Fišera <mara@neptuo.com>
2019-05-29 18:14:12 +0200

    #9 - UI for registration.

---
+++ src/Recollection.Blazor.UI/Accounts/Pages/Register.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Recollection.Accounts.Pages
+{
+    public class RegisterModel : ComponentBase
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public async Task RegisterAsync()
+        {
+            UserName = null;
+            Password = null;
+        }
+    }
+}
