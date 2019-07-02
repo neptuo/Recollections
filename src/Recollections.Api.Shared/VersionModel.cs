0999359d4f33ab46bb0f060341765f38db569004
Marek Fišera <mara@neptuo.com>
2019-07-02 19:58:11 +0200

    #46 -  Add endpoint providing current API version.

---
+++ src/Recollections.Api.Shared/VersionModel.cs
@@ -0,0 +1,28 @@
+﻿using Neptuo;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Recollections
+{
+    public class VersionModel
+    {
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public int Patch { get; set; }
+
+        public VersionModel()
+        { }
+
+        public VersionModel(Version model)
+        {
+            Ensure.NotNull(model, "model");
+            Major = model.Major;
+            Minor = model.Minor;
+            Patch = model.Build;
+        }
+    }
+}
---
+++ src/Recollections.Api/Entries/Controllers/VersionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Recollections.Entries.Controllers
+{
+    [ApiController]
+    [Route("api/entries/version")]
+    public class VersionController : ControllerBase
+    {
+        public ActionResult<VersionModel> Get() => Ok(new VersionModel(typeof(VersionController).Assembly.GetName().Version));
+    }
+}
