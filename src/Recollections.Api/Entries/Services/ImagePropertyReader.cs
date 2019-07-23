78ad258e301a0045f6d2669215c1a87397ebfebb
Marek Fišera <mara@neptuo.com>
2019-07-23 17:17:00 +0200

    #6 - Image GPS property reader.

---
+++ src/Recollections.Api/Entries/Services/ImagePropertyReader.cs
@@ -0,0 +1,41 @@
+﻿using ExifLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Recollections.Entries.Services
+{
+    public class ImagePropertyReader : DisposableBase
+    {
+        private readonly ExifReader reader;
+
+        public ImagePropertyReader(string imagePath) 
+            => reader = new ExifReader(imagePath);
+
+        public double? GetLatitude() 
+            => GetCoordinate(ExifTags.GPSLatitude);
+
+        public double? GetLongitude() 
+            => GetCoordinate(ExifTags.GPSLongitude);
+
+        private double? GetCoordinate(ExifTags type)
+        {
+            if (reader.GetTagValue(type, out double[] coordinates))
+                return ToDoubleCoordinates(coordinates);
+
+            return null;
+        }
+
+        private double ToDoubleCoordinates(double[] coordinates) 
+            => coordinates[0] + (coordinates[1] / 60f) + coordinates[2] / 3600f;
+
+        protected override void DisposeManagedResources()
+        {
+            base.DisposeManagedResources();
+            reader.Dispose();
+        }
+    }
+}
