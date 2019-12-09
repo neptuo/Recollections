5b1fc1a521b5b59d8be1fc37616e4e5f5e73db91
Marek Fišera <mara@neptuo.com>
2019-12-09 22:21:18 +0100

    #98 - Set debug PWA cache version to current timestamp.

--- src/Recollections.Blazor.UI/wwwroot/service-worker.js
+++ src/Recollections.Blazor.UI/wwwroot/service-worker.js
@@ -5,7 +5,7 @@ const swInstallEvent = 'install';
 const swInstalledEvent = 'installed';
 const swActivateEvent = 'activate';
 const staticCachePrefix = 'blazor-cache-v';
-const staticCacheName = 'blazor-cache-v1';
+const staticCacheName = 'blazor-cache-v0.0.637115268534426753';
 const requiredFiles = [
 "/_framework/blazor.boot.json",
 "/_framework/blazor.webassembly.js",
