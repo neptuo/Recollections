65b41959d0bd46dd4d6a16787fc6a5d54dc94f94
Marek Fišera <mara@neptuo.com>
2020-03-24 16:12:40 +0100

    #115 - Use SQL generated identity for entry.

--- src/Recollections.Entries.Data/Entry.cs
+++ src/Recollections.Entries.Data/Entry.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ namespace Neptuo.Recollections.Entries
     public class Entry
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
 
         public string UserId { get; set; }
