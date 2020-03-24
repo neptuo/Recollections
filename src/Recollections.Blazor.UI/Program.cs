b9c964d56cf83b0bec014559028fcb4ed955441e
Marek Fišera <mara@neptuo.com>
2020-03-24 13:12:22 +0100

    #115 - Update all packages and make the solution buildable.

--- src/Recollections.Blazor.UI/Program.cs
+++ src/Recollections.Blazor.UI/Program.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Neptuo.Events;
+using Neptuo.Recollections.Accounts;
+using Neptuo.Recollections.Commons;
+using Neptuo.Recollections.Entries;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,13 +14,67 @@ namespace Neptuo.Recollections
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static CommonStartup common;
+        private static AccountsStartup accounts;
+        private static EntriesStartup entries;
+
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // Startups
+            common = new CommonStartup();
+            accounts = new AccountsStartup();
+            entries = new EntriesStartup();
+
+            // Configure.
+            WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault();
+            ConfigureServices(builder.Services);
+            ConfigureComponents(builder.RootComponents);
+
+            // Startup.
+            WebAssemblyHost host = builder.Build();
+            StartupServices(host.Services);
+
+            // Run.
+            await host.RunAsync();
         }
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            DefaultEventManager eventManager = new DefaultEventManager();
+
+            services
+                .AddLogging()
+                .AddExceptions()
+                .AddComponents()
+                .AddUiOptions()
+                .AddHttpClientFactory()
+                .AddSingleton<IEventDispatcher>(eventManager)
+                .AddSingleton<IEventHandlerCollection>(eventManager)
+                .AddSingleton<Json>()
+                .AddSingleton<UrlResolver>(Resolve)
+                .AddTransient<Navigator>();
+
+            common.ConfigureServices(services);
+            accounts.ConfigureServices(services);
+            entries.ConfigureServices(services);
+        }
+
+        private static void ConfigureComponents(RootComponentMappingCollection rootComponents)
+        {
+            rootComponents.Add<App>("app");
+        }
+
+        private static void StartupServices(IServiceProvider services)
+        {
+        }
+
+        private static string Resolve(string appRelative)
+        {
+#if DEBUG
+            return $"http://localhost:33880/api{appRelative}";
+#else
+            return $"https://api.recollections.neptuo.com/api{appRelative}";
+#endif
+        }
     }
 }
