810230e76b99d855000fb1748c8701675b8e0a17
Marek Fišera <mara@neptuo.com>
2020-04-02 13:22:35 +0200

    #118 - Empty page for public profile.

---
+++ src/Recollections.Blazor.UI/Accounts/Pages/Profile.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Recollections.Accounts.Pages
+{
+    public partial class Profile
+    {
+        [Parameter]
+        public string UserName { get; set; }
+    }
+}
