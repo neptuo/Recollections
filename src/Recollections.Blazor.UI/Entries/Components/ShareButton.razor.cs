032327ee57396bf60d94538be176acc387d9915d
Marek Fišera <mara@neptuo.com>
2020-10-10 23:54:12 +0200

    #164 - Share button mockup.

---
+++ src/Recollections.Blazor.UI/Entries/Components/ShareButton.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Neptuo.Recollections.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Recollections.Entries.Components
+{
+    public partial class ShareButton
+    {
+        [Parameter]
+        public string EntryId { get; set; }
+
+        protected Modal Modal { get; set; }
+
+        protected void OnToggle()
+            => Modal.Show();
+    }
+}
