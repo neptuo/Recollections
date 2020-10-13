7c80f9f840f02e0cf8e3b58d88297e2f10d28679
Marek Fišera <mara@neptuo.com>
2020-10-13 15:42:04 +0200

    #116 - Add InlineLink editor.
    An inline-link editor that you bind click event on.

---
+++ src/Recollections.Blazor.Components/Components/Editors/InlineLink.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Recollections.Components.Editors
+{
+    public partial class InlineLink
+    {
+        [Parameter]
+        public string Icon { get; set; }
+
+        [Parameter]
+        public string IconPrefix { get; set; }
+
+        [Parameter]
+        public EventCallback OnClick { get; set; }
+
+        protected async override Task OnEditAsync()
+        {
+            await base.OnEditAsync();
+
+            if (IsEditMode)
+                await OnClick.InvokeAsync();
+
+            await OnResetAsync();
+        }
+    }
+}
