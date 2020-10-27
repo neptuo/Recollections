c4099ec09e855d0746bd7d796bf9a273aa772273
Marek Fišera <mara@neptuo.com>
2020-10-27 20:35:54 +0100

    #190 - A service for deleting shares.

---
+++ src/Recollections.Api/Sharing/ShareDeleter.cs
@@ -0,0 +1,22 @@
+﻿using Neptuo;
+using Neptuo.Recollections.Entries;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Recollections.Sharing
+{
+    public class ShareDeleter
+    {
+        private readonly DataContext db;
+
+        public ShareDeleter(DataContext db)
+        {
+            Ensure.NotNull(db, "db");
+            this.db = db;
+        }
+    }
+}
