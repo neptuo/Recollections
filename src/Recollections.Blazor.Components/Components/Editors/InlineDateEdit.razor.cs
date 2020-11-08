03e07b4accc6abc7065d0f7b302a2e5c7525eb2a
Marek Fišera <mara@neptuo.com>
2020-11-08 11:15:26 +0100

    #193 - Time selection support in InlineDateEdit.

--- src/Recollections.Blazor.Components/Components/Editors/InlineDateEdit.razor.cs
+++ src/Recollections.Blazor.Components/Components/Editors/InlineDateEdit.razor.cs
@@ -12,32 +12,53 @@ namespace Neptuo.Recollections.Components.Editors
 {
     public partial class InlineDateEdit
     {
+        protected const string TimeFormat = "HH:mm";
+
         [Inject]
         protected DatePickerInterop Interop { get; set; }
 
-        protected ElementReference? Input { get; set; }
+        protected ElementReference? DateInput { get; set; }
 
         [Parameter]
         public string Format { get; set; }
+
+        [Parameter]
+        public bool IsTimeSelection { get; set; }
+
+        protected string TimeValue { get; set; }
+
+        protected override void OnParametersSet() 
+        {
+            base.OnParametersSet();
+
+            if (IsTimeSelection)
+                TimeValue = Value.ToString(TimeFormat);
+        }
 
         protected async override Task OnAfterRenderAsync(bool firstRender)
         {
             await base.OnAfterRenderAsync(firstRender);
 
             if (IsEditMode)
-                await Interop.InitializeAsync(Input.Value, Format);
+                await Interop.InitializeAsync(DateInput.Value, Format);
         }
 
         protected async override Task OnValueChangedAsync()
         {
-            if (Input != null)
+            if (DateInput != null)
             {
-                string rawValue = await Interop.GetValueAsync(Input.Value);
+                string rawValue = await Interop.GetValueAsync(DateInput.Value);
                 if (DateTime.TryParseExact(rawValue, Format, CultureInfo.CurrentCulture, DateTimeStyles.None, out var value))
                     Value = value;
+
+                if (IsTimeSelection && TimeSpan.TryParse(TimeValue, out var time))
+                    Value = Value.Date.Add(time);
             }
 
             await base.OnValueChangedAsync();
         }
+
+        protected string GetDateCssClass()
+            => IsTimeSelection ? "inline-datetime" : "inline-date";
     }
 }
