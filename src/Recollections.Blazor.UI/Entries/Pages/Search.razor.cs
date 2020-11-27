2324cf0e829bf598c97f811a13b0fbd50d60248b
Marek Fišera <mara@neptuo.com>
2020-11-27 19:00:06 +0100

    #207 - Page template for searching using query string parameter.

---
+++ src/Recollections.Blazor.UI/Entries/Pages/Search.razor.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Recollections.Entries.Pages
+{
+    public partial class Search : IDisposable
+    {
+        [Inject]
+        protected Navigator Navigator { get; set; }
+
+        [Parameter]
+        public string Query { get; set; }
+
+        protected List<object> Items { get; set; }
+        protected bool IsLoading { get; set; }
+
+        protected string EmptyMessage
+            => String.IsNullOrEmpty(Query)
+                ? "Start by filling the search phrase..."
+                : $"Nothing matches '{Query}'...";
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Navigator.LocationChanged += OnLocationChanged;
+        }
+
+        public void Dispose()
+        {
+            Navigator.LocationChanged -= OnLocationChanged;
+        }
+
+        private async void OnLocationChanged(string url)
+        {
+            await SearchAsync();
+            StateHasChanged();
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            await base.OnParametersSetAsync();
+            await SearchAsync();
+        }
+
+        protected async Task SearchAsync()
+        {
+            Query = Navigator.FindQueryParameter("q");
+            if (String.IsNullOrEmpty(Query))
+                return;
+
+            try
+            {
+                IsLoading = true;
+                await Task.Delay(1000);
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+    }
+}
--- src/Recollections.Blazor.UI/Navigator.cs
+++ src/Recollections.Blazor.UI/Navigator.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
 using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +17,7 @@ namespace Neptuo.Recollections
     {
         private readonly NavigationManager uri;
         private readonly IJSRuntime jsRuntime;
+        private Dictionary<string, StringValues> queryString;
 
         public event Action<string> LocationChanged;
 
@@ -33,10 +37,32 @@ namespace Neptuo.Recollections
         }
 
         private void OnLocationChanged(object sender, LocationChangedEventArgs e)
-            => LocationChanged?.Invoke(e.Location);
+        {
+            queryString = null;
+            LocationChanged?.Invoke(e.Location);
+        }
 
         public ValueTask ReloadAsync()
             => jsRuntime.InvokeVoidAsync("window.location.reload");
+
+        public Dictionary<string, StringValues> GetQueryString()
+        {
+            if (queryString == null)
+            {
+                var absolute = uri.ToAbsoluteUri(uri.Uri);
+                queryString = QueryHelpers.ParseQuery(absolute.Query);
+            }
+
+            return queryString;
+        }
+
+        public string FindQueryParameter(string name)
+        {
+            if (GetQueryString().TryGetValue(name, out var value))
+                return value;
+
+            return null;
+        }
 
         public ValueTask<bool> AskAsync(string message)
             => jsRuntime.InvokeAsync<bool>("window.confirm", message);
@@ -79,6 +105,18 @@ namespace Neptuo.Recollections
 
         public void OpenMap()
             => uri.NavigateTo(UrlMap());
+
+        public string UrlSearch(string query = null)
+        {
+            string url = "/search";
+            if (!String.IsNullOrEmpty(query))
+                url = QueryHelpers.AddQueryString(url, "q", query);
+            
+            return url;
+        }
+
+        public void OpenSearch(string query = null)
+            => uri.NavigateTo(UrlSearch(query));
 
         public string UrlStories()
             => "/stories";
