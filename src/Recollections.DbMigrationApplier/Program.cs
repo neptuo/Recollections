7bf7698ae7349c0870e52c1f6232af3ebf087d8c
Marek Fišera <mara@neptuo.com>
2021-04-13 19:18:01 +0200

    #240 - Console application for migrating databases.

---
+++ src/Recollections.DbMigrationApplier/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using AccountsDataContext = Neptuo.Recollections.Accounts.DataContext;
+using EntriesDataContext = Neptuo.Recollections.Entries.DataContext;
+
+namespace Neptuo.Recollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Pass two arguments as two connection strings to two databases to migrate.");
+                return;
+            }
+
+            Console.WriteLine("Creating contexts.");
+            using var accounts = new AccountsDataContext(DbContextOptions<AccountsDataContext>(""), Schema<AccountsDataContext>("Accounts"));
+            using var entries = new EntriesDataContext(DbContextOptions<EntriesDataContext>(""), Schema<EntriesDataContext>("Entries"));
+
+            Console.WriteLine("Migrating accounts db.");
+            accounts.Database.Migrate();
+            
+            Console.WriteLine("Migrating entries db.");
+            entries.Database.Migrate();
+
+            Console.WriteLine("Done.");
+        }
+
+        private static DbContextOptions<T> DbContextOptions<T>(string connectionString)
+            where T : DbContext
+        {
+            return new DbContextOptionsBuilder<T>().UseSqlServer(connectionString).Options;
+        }
+
+        private static SchemaOptions<T> Schema<T>(string name)
+            where T : DbContext
+        {
+            return new SchemaOptions<T>() { Name = name };
+        }
+}
+}
