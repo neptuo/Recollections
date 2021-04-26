1f32e8c6a5aac1e10f983a23a329c7c8da62c7c9
Marek Fišera <mara@neptuo.com>
2021-04-26 11:04:40 +0200

    #244 - Show icons on small to medium screens, without text.

--- src/Recollections.Blazor.UI/Commons/Layouts/HeadLayout.razor.cs
+++ src/Recollections.Blazor.UI/Commons/Layouts/HeadLayout.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
 using Neptuo.Recollections.Accounts.Components;
 using Neptuo.Recollections.Components;
 using System;
@@ -15,12 +16,21 @@ namespace Neptuo.Recollections.Commons.Layouts
         [Inject]
         internal Navigator Navigator { get; set; }
 
+        protected List<MenuItem> MenuItems { get; } = new List<MenuItem>();
         protected ExceptionPanel ExceptionPanel { get; set; }
         protected bool IsMainMenuVisible { get; set; } = false;
 
         protected override void OnInitialized()
         {
             base.OnInitialized();
+
+            MenuItems.Add(new MenuItem("Timeline", "stream", Navigator.UrlTimeline(), NavLinkMatch.All));
+            MenuItems.Add(new MenuItem("Map", "map-marked-alt", Navigator.UrlMap()));
+            MenuItems.Add(new MenuItem("Calendar", "calendar-alt", Navigator.UrlCalendar()));
+            MenuItems.Add(new MenuItem("Search", "search", Navigator.UrlSearch()));
+            MenuItems.Add(new MenuItem("Stories", "book", Navigator.UrlStories()));
+            MenuItems.Add(new MenuItem("Beings", "user-friends", Navigator.UrlBeings()));
+            MenuItems.Add(new MenuItem("About", "info-circle", Navigator.UrlAbout(), isSmall: false));
 
             Navigator.LocationChanged += OnLocationChanged;
         }
@@ -47,5 +57,26 @@ namespace Neptuo.Recollections.Commons.Layouts
 
         protected void ToggleMainMenu()
             => UpdateMainMenuVisible(!IsMainMenuVisible);
+    }
+
+    public class MenuItem
+    {
+        public string Text { get; set; }
+        public string Icon { get; set; }
+        public string Url { get; set; }
+        public NavLinkMatch Match { get; set; }
+
+        public bool IsSmall { get; set; }
+        public bool IsLarge { get; set; }
+
+        public MenuItem(string text, string icon, string url, NavLinkMatch match = NavLinkMatch.Prefix, bool isSmall = true, bool isLarge = true)
+        {
+            Text = text;
+            Icon = icon;
+            Url = url;
+            Match = match;
+            IsSmall = isSmall;
+            IsLarge = isLarge;
+        }
     }
 }
