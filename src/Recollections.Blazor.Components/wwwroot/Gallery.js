065183442c5892c00f223c995656bd340dc37f86
Marek Fišera <mara@neptuo.com>
2021-05-08 00:52:09 +0200

    #71 - Show image title in gallery.

--- src/Recollections.Blazor.Components/wwwroot/Gallery.js
+++ src/Recollections.Blazor.Components/wwwroot/Gallery.js
@@ -8,6 +8,33 @@ const options = {
 const lightbox = new PhotoSwipeLightbox(options);
 
 export function initialize(interop, images) {
+    lightbox.on('uiRegister', function () {
+        lightbox.pswp.ui.registerElement({
+            name: 'test-button',
+            order: 9,
+            isButton: true,
+            html: 'Test',
+            onClick: (event, el) => {
+                if (confirm('Do you want to toggle zoom?')) {
+                    lightbox.pswp.toggleZoom();
+                }
+            }
+        });
+
+        lightbox.pswp.ui.registerElement({
+            name: 'title',
+            order: 9,
+            isButton: false,
+            appendTo: 'root',
+            html: 'Caption text',
+            onInit: (el, pswp) => {
+                lightbox.pswp.on('change', () => {
+                    el.innerHTML = lightbox.pswp.currSlide.data.alt || '';
+                });
+            }
+        });
+    });
+
     lightbox.on("numItems", (e) => {
         e.numItems = images.length
     });
