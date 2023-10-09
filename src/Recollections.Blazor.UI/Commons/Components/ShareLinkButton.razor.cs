033f40a1f4f1102727f8fc86aff489dfa9044474
Marek Fišera <mara@neptuo.com>
2023-10-09 23:14:37 +0200

    #286 - Share link button.
    Signed-off-by: Marek Fišera <mara@neptuo.com>

---
+++ src/Recollections.Blazor.UI/Commons/Components/ShareLinkButton.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Neptuo.Recollections.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Recollections.Commons.Components
+{
+    public partial class ShareLinkButton
+    {
+        [Inject]
+        protected Navigator Navigator { get; set; }
+
+        [Inject]
+        protected WindowInterop Interop { get; set; }
+
+        protected Modal Modal { get; set; }
+        protected bool IsCopiedToClipboard { get; set; }
+    }
+}
