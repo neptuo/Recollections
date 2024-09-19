f6f03b094926636057a00f850f7ab1bad6a439ce
Marek Fišera <mara@neptuo.com>
2024-09-19 19:09:54 +0200

    #267 - Render markdown URLs as links.

--- src/Recollections.Blazor.Components/MarkdownConverter.cs
+++ src/Recollections.Blazor.Components/MarkdownConverter.cs
@@ -1,24 +1,72 @@
 ﻿using CommonMark;
+using CommonMark.Syntax;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Neptuo.Recollections
+namespace Neptuo.Recollections;
+
+public class MarkdownConverter
 {
-    public class MarkdownConverter
+    private static readonly CommonMarkSettings settings;
+
+    static MarkdownConverter()
     {
-        private static readonly CommonMarkSettings settings;
+        settings = CommonMarkSettings.Default.Clone();
+        settings.RenderSoftLineBreaksAsLineBreaks = true;
+        settings.AdditionalFeatures |= CommonMarkAdditionalFeatures.StrikethroughTilde;
+        settings.OutputDelegate = (doc, output, settings) => new CustomHtmlFormatter(output, settings).WriteDocument(doc);
+    }
 
-        static MarkdownConverter()
+    public string Convert(string markdown) => CommonMarkConverter.Convert(markdown, settings);
+}
+
+class CustomHtmlFormatter : CommonMark.Formatters.HtmlFormatter
+{
+    public CustomHtmlFormatter(TextWriter target, CommonMarkSettings settings)
+        : base(target, settings)
+    {
+    }
+
+    private Regex urlRegex = new Regex(@"(?<Protocol>\w+):\/\/(?<Domain>[\w@][\w.:@]+)\/?[\w\.?=%&=\-@/$,]*");
+
+    protected override void WriteInline(Inline inline, bool isOpening, bool isClosing, out bool ignoreChildNodes)
+    {
+        if (inline.Tag == InlineTag.String)
         {
-            settings = CommonMarkSettings.Default.Clone();
-            settings.RenderSoftLineBreaksAsLineBreaks = true;
-            settings.AdditionalFeatures |= CommonMarkAdditionalFeatures.StrikethroughTilde;
-        }
+            Match match = urlRegex.Match(inline.LiteralContent);
+            if (match.Success)
+            {
+                var lastIndex = 0;
+                while (match.Success)
+                {
+                    Write(inline.LiteralContent.Substring(lastIndex, match.Index));
+                    Write($"<a href='");
+                    WriteEncodedUrl(match.Value);
+                    Write($"' target='_blank'>{match.Value}</a>");
 
-        public string Convert(string markdown) => CommonMarkConverter.Convert(markdown, settings);
+                    lastIndex = match.Index + match.Length;
+
+                    match = match.NextMatch();
+                }
+
+                if (lastIndex < inline.LiteralContent.Length)
+                    Write(inline.LiteralContent.Substring(lastIndex, inline.LiteralContent.Length - lastIndex));
+
+                ignoreChildNodes = false;
+            }
+            else
+            {
+                base.WriteInline(inline, isOpening, isClosing, out ignoreChildNodes);
+            }
+        }
+        else
+        {
+            base.WriteInline(inline, isOpening, isClosing, out ignoreChildNodes);
+        }
     }
 }
