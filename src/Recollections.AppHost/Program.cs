aaf9fa6cd81093d484fdce64e1c4be0c63e46a11
Marek Fišera <mara@neptuo.com>
2024-11-04 13:58:25 +0100

    #342 - Start Blazor as executable when using dotnet watch

--- src/Recollections.AppHost/Program.cs
+++ src/Recollections.AppHost/Program.cs
@@ -1,10 +1,23 @@
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder
     .AddProject<Projects.Recollections_Api>("api");
 
-var uiService = builder
-    .AddProject<Projects.Recollections_Blazor_UI>("ui")
-    .WithReference(apiService);
+if (builder.Configuration.GetValue<string>("DOTNET_WATCH") == "1")
+{
+    var ui = builder
+        .AddExecutable("ui", "dotnet", Path.GetDirectoryName(new Projects.Recollections_Blazor_UI().ProjectPath)!, ["watch", "--non-interactive"])
+        .WithHttpEndpoint(targetPort: 33881, isProxied: false)
+        .WithReference(apiService);
+}
+else
+{
+    var ui = builder
+        .AddProject<Projects.Recollections_Blazor_UI>("ui")
+        .WithReference(apiService);
+}
 
 builder.Build().Run();
