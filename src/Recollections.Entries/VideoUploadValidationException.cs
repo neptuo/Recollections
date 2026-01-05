6e54bda5ac458523fa738121edc9a203feb8b5d5
Marek Fišera <mara@neptuo.com>
2026-01-05 13:26:31 +0100

    Using Xabe ffmpeg

--- src/Recollections.Entries/VideoUploadValidationException.cs
+++ src/Recollections.Entries/VideoUploadValidationException.cs
@@ -7,5 +7,9 @@ namespace Neptuo.Recollections.Entries
         public VideoUploadValidationException(string message = null)
             : base(message)
         { }
+
+        public VideoUploadValidationException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
     }
 }
