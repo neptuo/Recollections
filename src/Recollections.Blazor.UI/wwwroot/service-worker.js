30514b459bec68377235b7e0200bdd1e59d3399e
Marek Fišera <mara@neptuo.com>
2026-01-07 14:11:29 +0100

    #322 - Manifest and development share boilerplate

--- src/Recollections.Blazor.UI/wwwroot/service-worker.js
+++ src/Recollections.Blazor.UI/wwwroot/service-worker.js
@@ -1,7 +1,11 @@
 ﻿// In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
+self.addEventListener('fetch', event => {
+    if (event.request.method === "POST") {
+        return handleShareTarget(event);
+    }
+});
 
 self.addEventListener('message', onMessage);
 
@@ -9,4 +13,28 @@ function onMessage(event) {
     if (event.data.action === 'skipWaiting') {
         self.skipWaiting();
     }
+}
+
+async function handleShareTarget(e) {
+    const formData = await e.request.formData();
+    const files = formData.getAll("allfiles");
+    const url = e.request.url;
+
+    if (files && files.length > 0) {
+        let fileObjects = [];
+        for (let i = 0; i < files.length; i++) {
+            fileObjects.push({
+                entryId: null,
+                name: files[i].name,
+                buffer: await files[i].arrayBuffer()
+            });
+        }
+
+        // TODO: Pass to UI thread or store in DB
+        for (var i = 0; i < fileObjects.length; i++) {
+            console.log(fileObjects[i].name);
+        }
+    }
+
+    return Response.redirect(url, 302);
 }
