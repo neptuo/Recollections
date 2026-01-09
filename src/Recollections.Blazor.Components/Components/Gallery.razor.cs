851293d7691af657a21846219650bf9607490222
Marek Fišera <mara@neptuo.com>
2026-01-09 11:27:05 +0100

    #389 - Skip gallery rendering if items are unchanged

--- src/Recollections.Blazor.Components/Components/Gallery.razor.cs
+++ src/Recollections.Blazor.Components/Components/Gallery.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.JSInterop;
+using Neptuo.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,12 +12,9 @@ using System.Threading.Tasks;
 
 namespace Neptuo.Recollections.Components
 {
-    public partial class Gallery : IAsyncDisposable
+    public partial class Gallery(ILog<Gallery> Log, GalleryInterop Interop) : IAsyncDisposable
     {
-        private int? indexToOpen;
-
-        [Inject]
-        protected GalleryInterop Interop { get; set; }
+        private int previousParametersHashCode;
 
         [Parameter]
         public List<GalleryModel> Models { get; set; }
@@ -27,18 +25,37 @@ namespace Neptuo.Recollections.Components
         [Parameter]
         public EventCallback<int> OnOpenInfo { get; set; }
 
+        private int ComputeParametersHashCode()
+        {
+            var hashCode = new HashCode();
+
+            hashCode.Add(Models ?? []);
+            if (Models != null)
+            {
+                hashCode.Add(Models.Count);
+                foreach (var model in Models)
+                    hashCode.Add(model);
+            }
+
+            return hashCode.ToHashCode();
+        }
+
         protected async override Task OnAfterRenderAsync(bool firstRender)
         {
             await base.OnAfterRenderAsync(firstRender);
-            await Interop.InitializedAsync(this, Models ?? new List<GalleryModel>(0));
+            await Interop.InitializedAsync(this, Models ?? []);
 
-            if (indexToOpen != null)
-            {
-                if (Models.Count > indexToOpen.Value)
-                    await Interop.OpenAsync(indexToOpen.Value);
+            // 'Models' is passed as reference, so when SetParametersAsync is called, it already contains a new items.
+            // Because of that it's important we compute the 'previous' value at the end of the rendering cycle,
+            // because there is no place in the beginning.
+            previousParametersHashCode = ComputeParametersHashCode();
+        }
 
-                indexToOpen = null;
-            }
+        protected override bool ShouldRender()
+        {
+            var result = previousParametersHashCode != ComputeParametersHashCode();
+            Log.Debug($"ShouldRender: result = '{result}'");
+            return result;
         }
 
         protected async Task OnBeforeInternalNavigation(LocationChangingContext context)
@@ -50,8 +67,13 @@ namespace Neptuo.Recollections.Components
             }
         }
 
-        public void Open(int index) 
-            => indexToOpen = index;
+        public Task OpenAsync(int index)
+        {
+            if (Models.Count > index)
+                return Interop.OpenAsync(index);
+
+            return Task.CompletedTask;
+        }
 
         public Task CloseAsync()
             => Interop.CloseAsync();
--- src/Recollections.Blazor.Components/Components/GalleryModel.cs
+++ src/Recollections.Blazor.Components/Components/GalleryModel.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Neptuo.Recollections.Components
 {
-    public class GalleryModel
+    public class GalleryModel : IEquatable<GalleryModel>
     {
         /// <summary>
         /// Optional discriminator, e.g. "image" or "video".
@@ -19,5 +19,18 @@ namespace Neptuo.Recollections.Components
         public int Height { get; set; }
 
         public string ContentType { get; set; }
+
+        public override int GetHashCode()
+            => HashCode.Combine(Type, Title, Width, Height, ContentType);
+
+        public override bool Equals(object obj)
+            => (obj is GalleryModel other) && Equals(other);
+
+        public bool Equals(GalleryModel other)
+            => Type == other.Type
+                && Title == other.Title
+                && Width == other.Width
+                && Height == other.Height
+                && ContentType == other.ContentType;
     }
 }
