cdc89d59ebe48d04fdd1d6c7ee9917e9871b621e
Marek Fišera <mara@neptuo.com>
2026-01-13 09:39:19 +0100

    #253 - Offcanvas lazy body render

--- src/Recollections.Blazor.Components/Components/Offcanvas.razor.cs
+++ src/Recollections.Blazor.Components/Components/Offcanvas.razor.cs
@@ -11,6 +11,9 @@ namespace Neptuo.Recollections.Components;
 public partial class Offcanvas : System.IDisposable
 {
     protected ElementReference Element { get; set; }
+    private string cssClass;
+    private string bodyCssClass;
+    private bool hasBodyRendered;
 
     [Inject]
     protected ILog<Offcanvas> Log { get; set; }
@@ -42,6 +45,9 @@ public partial class Offcanvas : System.IDisposable
     [Parameter]
     public string Direction { get; set; }
 
+    [Parameter]
+    public bool IsBodyRenderedLazily { get; set; } = false;
+
     public bool IsVisible { get; private set; }
 
     protected override void OnParametersSet()
@@ -50,16 +56,14 @@ public partial class Offcanvas : System.IDisposable
 
         Attributes ??= [];
 
-        var cssClass = "offcanvas";
+        cssClass = "offcanvas";
         if (!string.IsNullOrEmpty(Direction))
             cssClass += " offcanvas-" + Direction;
 
         if (Attributes.TryGetValue("class", out var providedCssClass))
             cssClass += " " + providedCssClass;
 
-        Attributes["class"] = cssClass;
-
-        BodyCssClass = string.IsNullOrEmpty(BodyCssClass) ? "offcanvas-body" : $"offcanvas-body {BodyCssClass}";
+        bodyCssClass = string.IsNullOrEmpty(BodyCssClass) ? "offcanvas-body" : $"offcanvas-body {BodyCssClass}";
     }
 
     protected async override Task OnAfterRenderAsync(bool firstRender)
